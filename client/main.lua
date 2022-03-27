f02dddec2cd3c34fbeff59ea468314ebfe64f60a
Barney <98829127+BarneyWasTaken@users.noreply.github.com>
2022-03-27 21:31:07 +0200

    Update main.lua
    tiny detail

--- client/main.lua
+++ client/main.lua
@@ -1,6 +1,6 @@
 ESX = nil
 
-Citizen.CreateThread(function()
+CreateThread(function()
     while ESX == nil do
         TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end)
         Citizen.Wait(0)
