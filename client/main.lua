450487e2be16caeb300894fcec503ccf606537bc
Barney <98829127+BarneyWasTaken@users.noreply.github.com>
2022-03-27 21:32:05 +0200

    Update main.lua

--- client/main.lua
+++ client/main.lua
@@ -3,7 +3,7 @@ ESX = nil
 CreateThread(function()
     while ESX == nil do
         TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end)
-        Citizen.Wait(0)
+        Wait(0)
     end
 end)
 
